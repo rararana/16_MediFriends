cd273517c19021333d90f4e903f762a3a4248f61
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-08-24 23:59:45 +0700

    feat: added password hashing and create user to db

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,6 +2,8 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import { db } from "../lib/db";
+import bcrypt from "bcrypt";
 
 export const register = async (values: any) => {
 	const validatedFields = RegisterSchema.safeParse(values);
@@ -9,5 +11,29 @@ export const register = async (values: any) => {
 	if (!validatedFields.success) {
 		return { error: "Invalid fields" };
 	}
+
+	const { email, password, name } = validatedFields.data;
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const existingUser = await db.user.findUnique({
+		where: {
+			email,
+		},
+	});
+
+	if (!existingUser) {
+		return { error: "Email already in use!" };
+	}
+
+	await db.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	// Send verification token email
+
 	return { success: "Account Created! Enjoy your time on MediFriends!" };
 };
