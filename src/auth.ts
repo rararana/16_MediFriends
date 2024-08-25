9749fc39bc55ba95e611ff03c015cbf6a0e963e3
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-08-25 14:15:11 +0700

    test: added auth route file

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 
 export const {
 	handlers: { GET, POST },
 	auth,
 } = NextAuth({
-	providers: [GitHub],
+	providers: [
+		GitHubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
+	],
 });
