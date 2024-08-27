86b1a31fb5e1fbca202d6e09c1b7a029e4eefcef
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-08-27 17:59:29 +0700

    fix: next auth middle ware bug

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,6 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { getUserByEmail } from "@/data/user";
 import { LoginSchema } from "./schemas";
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ import {
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth(async (req) => {
 	const { nextUrl } = req;
 	const isLoggedIn = !!req.auth;
 
@@ -36,5 +36,10 @@ export default auth((req) => {
 });
 
 export const config = {
-	matcher: ["/((?!.+\\.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: [
+		// Skip Next.js internals and all static files, unless found in search params
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		// Always run for API routes
+		"/(api|trpc)(.*)",
+	],
 };
