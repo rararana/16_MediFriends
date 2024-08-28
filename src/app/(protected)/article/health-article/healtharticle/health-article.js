197d367de43f18420b0ed425891882c8c300c017
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-08-28 22:04:57 +0700

    feat: updated disease article

--- src/app/(protected)/article/health-article/healtharticle/health-article.js
+++ src/app/(protected)/article/health-article/healtharticle/health-article.js
@@ -1,39 +1,48 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const ArticleCard = ({ article }) => {
-  return (
-    <div style={styles.card}>
-      <Link href={article.link} passHref>
-          <Image src={article.image} alt={article.title} width={300} height={200} style={styles.image} />
-          <h3 style={styles.title}>{article.title}</h3>
-      </Link>
-    </div>
-  );
+	return (
+		<div
+			className="hover:scale-110 transition duration-500"
+			style={styles.card}
+		>
+			<Link href={article.link} passHref>
+				<Image
+					src={article.image}
+					alt={article.title}
+					width={300}
+					height={200}
+					style={styles.image}
+				/>
+				<h3 style={styles.title}>{article.title}</h3>
+			</Link>
+		</div>
+	);
 };
 
 const styles = {
-  card: {
-    border: '1px solid #ccc',
-    borderRadius: '8px',
-    padding: '16px',
-    textAlign: 'center',
-    margin: '16px',
-    width: '300px',
-  },
-  link: {
-    textDecoration: 'none',
-    color: 'inherit',
-  },
-  image: {
-    borderRadius: '8px',
-  },
-  title: {
-    marginTop: '12px',
-    fontSize: '18px',
-    fontWeight: 'bold',
-  },
+	card: {
+		border: "1px solid #ccc",
+		borderRadius: "8px",
+		padding: "16px",
+		textAlign: "center",
+		margin: "16px",
+		width: "300px",
+	},
+	link: {
+		textDecoration: "none",
+		color: "inherit",
+	},
+	image: {
+		borderRadius: "8px",
+	},
+	title: {
+		marginTop: "12px",
+		fontSize: "18px",
+		fontWeight: "bold",
+	},
 };
 
 export default ArticleCard;
