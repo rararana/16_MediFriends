4e62d0e89c00e4814a8924dd1ceefa140fbc8b99
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-08-28 23:54:04 +0700

    Update page.jsx

--- src/app/(protected)/visit-history/page.jsx
+++ src/app/(protected)/visit-history/page.jsx
@@ -1,8 +1,7 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardList from "../../../components/VisitHistory/CardList";
 import Form from "../../../components/VisitHistory/Form";
-import { useEffect, useState } from "react";
 import MobileNav from "@/components/MobileNav";
 import NavDashboard from "@/components/NavDashboard";
 import { useSession } from "next-auth/react";
@@ -10,9 +9,8 @@ import { useSession } from "next-auth/react";
 const Datas = () => {
 	const [datas, setDatas] = useState([]);
 	const session = useSession();
-	console.log(JSON.stringify(session));
-
 	const [nav, setNav] = useState(false);
+
 	const openNav = () => setNav(true);
 	const closeNav = () => setNav(false);
 
@@ -22,24 +20,19 @@ const Datas = () => {
 			if (!response.ok) {
 				throw new Error("Network response was not ok");
 			}
-			console.log(response);
-			const dataa = await response.json(); // Parsing the response as JSON
-			setDatas(dataa.allVisitHistory || []); // Updating the state with the fetched
+			const dataa = await response.json();
+			setDatas(dataa.allVisitHistory || []);
 		} catch (error) {
 			console.error("Failed to fetch data:", error);
 		}
 	};
 
 	useEffect(() => {
-		const triggerFetch = async () => {
-			await fetchData();
-		};
-		triggerFetch();
+		fetchData();
 	}, []);
 
 	return (
 		<>
-			{/* Nav */}
 			<MobileNav nav={nav} closeNav={closeNav} />
 			<NavDashboard openNav={openNav} closeNav={closeNav} />
 			<div className="mt-[6rem]">
@@ -47,20 +40,18 @@ const Datas = () => {
 					Data Kunjungan Rumah Sakit
 				</h1>
 				<Form />
-				{datas.map((data) => {
-					return (
-						<CardList>
-							<div className="ml-5 font-semibold rounded-md text-xl mb-3 tracking-wider">
-								{data.clinicHospitalName}
-							</div>
-							<div className="leading-relaxed mx-5 my-1 pl-5 rounded-md bg-sky-200 bg-opacity-40 text-lg border-sky-100 border-opacity-20 border-2 py-3">
-								<div>{data.visitDate}</div>
-								<div>{data.diagnose}</div>
-								<div>{data.treatment}</div>
-							</div>
-						</CardList>
-					);
-				})}
+				{datas.map((data, index) => (
+					<CardList key={data.id || index}>
+						<div className="ml-5 font-semibold rounded-md text-xl mb-3 tracking-wider">
+							{data.clinicHospitalName}
+						</div>
+						<div className="leading-relaxed mx-5 my-1 pl-5 rounded-md bg-sky-200 bg-opacity-40 text-lg border-sky-100 border-opacity-20 border-2 py-3">
+							<div>{data.visitDate}</div>
+							<div>{data.diagnose}</div>
+							<div>{data.treatment}</div>
+						</div>
+					</CardList>
+				))}
 			</div>
 		</>
 	);
