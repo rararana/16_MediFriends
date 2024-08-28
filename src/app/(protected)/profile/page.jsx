d16a1a365f76b5c7960315eaa15f9200a43772bd
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-08-28 23:44:36 +0700

    Update page.jsx

--- src/app/(protected)/profile/page.jsx
+++ src/app/(protected)/profile/page.jsx
@@ -42,7 +42,7 @@ export default function Profile() {
 
 	const user = {
 		name: "John Doe",
-		height: 0,
+		height: 175,
 		weight: 70,
 		bmi: bmi || 22.9,
 		chronicDisease: "None",
@@ -97,7 +97,7 @@ export default function Profile() {
 									/>
 									<InfoItem
 										label="Height"
-										value={height || ""}
+										value={height || `${user.height} cm`}
 										isEditing={isEditing}
 										onChange={(e) =>
 											setHeight(e.target.value)
